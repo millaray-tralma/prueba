591ba7c85c0fc9dc074b64907dc4fbaf039203e2
Millaray Herrera Tralma <mitralmah@ine.gob.cl>
2023-12-14 16:22:57 -0300

    summary

--- fichero1.R
+++ fichero1.R
@@ -1,9 +1,15 @@
 #subo de prueba
 ghauxhgiaushxiua
 
+summary(cars)
 
 
 
 
+#lleva a la paghina de tokens y se le da la descripción y cuanto quiere que dure el token, hay que copiarlo porque no aparece nuevamente
+usethis::create_github_token()
 
 
+gitcreds::gitcreds_set()
+#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
+fddffff
